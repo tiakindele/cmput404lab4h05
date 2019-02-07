75924e3f57547f1940be8b289fd2491ac4797cd9
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2019-02-07 00:39:50 -0700

    commits

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tiakindele/Documents/WNTR19/CMPUT404/Labs/Lab4/cmput404lab4h05/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
